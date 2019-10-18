8abe7685084e70b849de7befce3f654201f03dc0
stacy867 <stacymurenzi@gmail.com>
2019-10-18 20:15:42 +0200

    created path

---
+++ ig/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^new/newprofile$', views.new_profile, name='new-profile'),
+    url(r'^new/profile$', views.profile, name='profile'),
+    url(r'^post/(\d+)',views.post,name ='post'),
+    url(r'^search/', views.search_results, name='search_results')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
