b7210e5366162a62bf33677f034d2d7ebb6c28f9
stacy867 <stacymurenzi@gmail.com>
2019-10-18 20:16:19 +0200

    created classes

--- ig/models.py
+++ ig/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 
     
 
 class Profile(models.Model):
-    bio= models.CharField(max_length =30,null=True)
+    # bio= models.CharField(max_length =30,null=True)
+    bio= HTMLField()
     profile_photo=models.ImageField(upload_to ='photos/',null=True)
-    user_id=models.ForeignKey(User,on_delete=models.CASCADE) 
+    user=models.ForeignKey(User,on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.bio
@@ -20,20 +22,26 @@ class Profile(models.Model):
         self.delete()     
 
     def update_profile(self):
-        self.update() 
+        self.update()
+        
+    @classmethod
+    def search_by_profile(cls,username):
+        certain_user = cls.objects.filter(user_id__username__icontains=username)
+        # profile = Profile.objects.filter(user__username__icontains=name)
+        return  certain_user     
 
 class Image(models.Model):
     image = models.ImageField(upload_to ='images/',null=True)
     image_name= models.CharField(max_length =30)
     image_caption= models.CharField(max_length =30,null=True)
-    likes= models.IntegerField()
+    likes= models.IntegerField(null=True)
     comments= models.CharField(max_length =30,null=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     profile=models.ForeignKey(Profile,null=True)
 
 
     def __str__(self):
-        return self.image_name
+        return self.image_caption
 
     def save_image(self):
         self.save()
